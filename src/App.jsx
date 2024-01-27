5e8835dbae92dbce46d21d85e53dfeb9466f6d54
Pedro Negrao <macedopedro117@yahoo.com.br>
2024-01-27 15:02:47 -0300

    Testando atualização de código

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import style from "./style/app.module.css";
 
 function App() {
   return (
-    <div className={style.page}>
-      <Container
-        avatar={imgProfile}
-        name="John Doe"
-        carrer="Full Stack Developer"
-        phone="+55 (21) 92121-4545"
-        email="john.doe@gmail.com"
-        githubUrl="https://github.com/"
-        linkedinUrl="https://www.linkedin.com/home"
-        twitterUrl="https://twitter.com/i/flow/login"
-      />
-    </div>
+    <>
+      <h1>Testando</h1>
+      <div className={style.page}>
+        <Container
+          avatar={imgProfile}
+          name="John Doe"
+          carrer="Full Stack Developer"
+          phone="+55 (21) 92121-4545"
+          email="john.doe@gmail.com"
+          githubUrl="https://github.com/"
+          linkedinUrl="https://www.linkedin.com/home"
+          twitterUrl="https://twitter.com/i/flow/login"
+        />
+      </div>
+    </>
   );
 }
 
